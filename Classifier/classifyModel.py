cb434e83d1ac459bed40f663c256395d48301367
ehwayoung31 <ehwayoung31@gmail.com>
2020-07-11 19:07:36 +0900

    requirements.txt  update

--- Classifier/classifyModel.py
+++
@@ -1,9 +0,0 @@
-from fastai.vision import *
-defaults.device = torch.device('cpu')
-img = image_file
-
-path=Path('classifier/data')
-learn = load_learner(path)
-
-pred_class,pred_idx,outputs = learn.predict(img)
-pred_class
